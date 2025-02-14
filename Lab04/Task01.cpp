31ed6ec0998376af189bbac8c5901bc9f6b894ce
maryam746 <solivagantmaryam26@gmail.com>
2025-02-14 13:09:34 +0500

    Update Task01.cpp

--- Lab04/Task01.cpp
+++ Lab04/Task01.cpp
@@ -1,1 +1,76 @@
+#include<iostream>
+using namespace std;
 
+class Car{
+	string brand;
+	string model;
+	float rentalprice;
+	bool availability;
+	
+	public:
+		Car(){
+			brand = "Unknown";
+			model = "Generic";
+			rentalprice=0.0;
+			availability = true;
+		
+		}
+		
+		void rentalrequest(){
+			string c;
+			
+			cout<<"Enter car name for rent:"<<endl;
+			getline(cin,c);
+			
+			if( check() == true ){
+				cout<<"Car is avaiable for rent";
+				availability = false; 
+				
+			}
+		
+			}
+			
+		
+		
+		bool check(){
+			if(availability == true){
+				cout<<"Car is available"<<endl;
+				return true;
+				
+			}
+			else{
+				cout<<"Car is not available"<<endl;
+		        return false;
+			}
+			
+		}
+		
+		void setb(string b){
+			brand=b;
+		}
+		
+		void setm(string m){
+			model=m;
+		}
+	
+	void update(){
+		string bran;
+		string mod;
+		setb("Toyota");
+		
+		setm("Aqua");
+		
+		
+		
+	}
+	
+	
+};
+
+
+
+
+int main(){
+	Car c1;
+	c1.update();
+}
