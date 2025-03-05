f753050e77f182e09c86b98ac03313f867834f2d
maryam746 <solivagantmaryam26@gmail.com>
2025-03-05 08:24:38 +0500

    Update Task01.cpp

--- Lab 06/Task01.cpp
+++ Lab 06/Task01.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+    string name;
+    float salary;
+
+    public:
+    Employee(string n ,float s):name(n),salary(s){}
+    
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Salary: "<<salary<<endl;
+    }
+
+
+    
+};
+
+class Manager: public Employee{
+    float bonus;
+
+    public:
+    Manager(string n,float s,float b):Employee(n,s),bonus(b){}
+
+    void display(){
+        Employee::display();
+        cout<<"Bonus: "<<bonus<<endl;
+    }
+};
+
+
+int main(){
+
+    Manager Hr("Ali",50000,10000);
+    Hr.display();
+    return 0;
+
+}
 
